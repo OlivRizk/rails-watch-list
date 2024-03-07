5b8294337f70ca93f0021616d5ba0fc544019658
Olivier Rizk <oliv.rizk@gmail.com>
2024-03-07 16:45:25 +0100

    watch list pas terminé, bloqué au bookmark

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,17 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.save
+    redirect_to list_bookmark_path(@bookmark)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
